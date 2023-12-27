0289f4a06e1b4c9d42dba79b54e2274b3c3bd743
Sulaymonqori <ms13061312@gmail.com>
2023-12-27 20:21:51 +0500

    task1

--- task1.py
+++ task1.py
@@ -1,1 +1,46 @@
-url = 'http://164.92.64.76/desc/'
+import os
+import httpx
+
+
+# class FileManager:
+#     def __init__(self, path, mode):
+#         self.path = path
+#         self.mode = mode
+#
+#     def __enter__(self):
+#         file = open(self.path, self.mode)
+#         self.file = file
+#         return self.file
+#
+#     def __exit__(self):
+#         self.file.close()
+
+class ContextManager:
+    def __init__(self, path, mode):
+        self.path = path
+        self.mode = mode
+
+    def __enter__(self):
+        file = open(self.path, self.mode)
+        self.file = file
+        return file
+
+    def __exit__(self):
+        self.file.close()
+
+
+if __name__ == '__main__':
+    files = os.listdir('descriptions')
+    os.chdir("descriptions")
+    print(files)
+
+    for file in files:
+        with ContextManager(file, 'r') as fin:
+            data = fin.readlines()
+            print(data)
+            # json_data = {
+                # "name":
+            # }
+
+            # with httpx.Client() as client:
+            #     response = client.post('http://164.92.64.76/desc/', )
